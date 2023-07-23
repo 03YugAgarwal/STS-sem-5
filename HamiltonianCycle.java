40fa903c0dc873370fa9f3c2ed82b93adcfeed01
Yug <03yugagarwal@gmail.com>
2023-07-23 21:16:26 +0530

    Hamiltonian, Manacher and StoredPermutation

---
+++ HamiltonianCycle.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class HamiltonianCycle {
+
+    final int V = 5;
+
+    boolean isSafe(int v, int graph[][], int path[], int pos) {
+        if (graph[path[pos - 1]][v] == 0)
+            return false;
+
+        for (int i = 0; i < pos; i++)
+            if (path[i] == v)
+                return false;
+
+        return true;
+    }
+
+    boolean hamCycleUtil(int graph[][], int path[], int pos) {
+        if (pos == V) {
+            if (graph[path[pos - 1]][path[0]] == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        for (int v = 1; v < V; v++) {
+            if (isSafe(v, graph, path, pos)) {
+                path[pos] = v;
+                if (hamCycleUtil(graph, path, pos + 1) == true) {
+                    return true;
+                }
+                path[pos] = -1;
+            }
+        }
+        return false;
+    }
+
+    int hamCycle(int graph[][]) {
+        int[] path = new int[V];
+        for (int i = 0; i < V; i++) {
+            path[i] = -1;
+        }
+        path[0] = 0;
+        if (hamCycleUtil(graph, path, 1) == false) {
+            return 0;
+        }
+        for (int i = 0; i < V; i++)
+            System.out.print(" " + path[i] + " ");
+        System.out.println(" " + path[0] + " ");
+        return 1;
+    }
+
+    public static void main(String args[]) {
+        int graph1[][] = {
+            { 0, 1, 0, 1, 0 },
+            { 1, 0, 1, 1, 1 },
+            { 0, 1, 0, 0, 1 },
+            { 1, 1, 0, 0, 1 },
+            { 0, 1, 1, 1, 0 },
+        };
+
+        HamiltonianCycle hc = new HamiltonianCycle();
+        hc.hamCycle(graph1);
+    }
+}
